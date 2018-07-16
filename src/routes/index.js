df95eeb443fa3563b738c0bb58fa2a213148e72f
revdancatt <github@revdancatt.com>
2018-07-16 20:32:17 +0100

    Boiler plate for minimal site

---
+++ src/routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const Config = require('../classes/config')
+
+// Break out all the seperate parts of the site
+/* eslint-disable import/no-unresolved */
+const main = require('./main')
+
+// ############################################################################
+//
+/*
+ * Always create a templateValues object that gets passed to the
+ * templates. The config object from global (this allows use to
+ * manipulate it here if we need to) and the user if one exists
+ */
+//
+// ############################################################################
+router.use(function (req, res, next) {
+  req.templateValues = {}
+  const configObj = new Config()
+  req.config = configObj
+  req.templateValues.config = req.config
+  req.templateValues.NODE_ENV = process.env.NODE_ENV
+  next()
+})
+
+// ############################################################################
+//
+//  Here are all the main routes
+//
+// ############################################################################
+
+router.get('/', main.index)
+
+
+module.exports = router
