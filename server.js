c3ade2f36a6df6d9709ab09eb0e7905c4f49fd51
revdancatt <github@revdancatt.com>
2018-07-16 19:27:51 +0100

    Babel and all the packages

---
+++ server.js
@@ -0,0 +1,11 @@
+/*
+ * Welcome to the server.js file, this file. This file is written in
+ * ES6 and is expected to run in node.
+ *
+ * There is a lot of scripting at the top of this file, most of which
+ * is to make sure the user has completed all the steps needed to
+ * actually run the dashboard properly. This will be checking for
+ * things like `yarn install` and the usual stuff having been run.
+ *
+ * You'll see!
+ */
