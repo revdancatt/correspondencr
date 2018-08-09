7220ab06f03efda7726258c252d8c3e4733893eb
revdancatt <github@revdancatt.com>
2018-08-09 17:23:13 +0100

    Allow us to add contacted information

--- src/routes/person/index.js
+++ src/routes/person/index.js
@@ -38,26 +38,55 @@ exports.update = (req, res) => {
   const person = new Person(parseInt(req.params.id, 10))
   if (person.id === undefined) return res.redirect('/')
 
-  //  Find out if we are doing notes or details
-  if ('notes' in req.body) {
-    person.set('notes', req.body.notes)
-  } else {
-    person.set('fullname', req.body.fullname)
-    if (('idontknowbirthday' in req.body && req.body.idontknowbirthday === 'true') || req.body.day === '') {
-      person.set('month', null)
-      person.set('day', null)
-    } else {
-      person.set('month', parseInt(req.body.month, 10))
-      person.set('day', parseInt(req.body.day, 10))
+  if ('action' in req.body) {
+    //  Find out if we are saving notes
+    if (req.body.action === 'Save notes') {
+      if (!('notes' in req.body)) req.body.notes = ''
+      person.set('notes', req.body.notes)
     }
-    person.set('address', req.body.address)
-    person.set('country', req.body.country)
 
-    const nameSplit = req.body.fullname.split(' ')
-    if (nameSplit.length > 0) person.set('firstname', nameSplit[0])
-    if (nameSplit.length > 1) person.set('lastname', nameSplit[1])
+    //  If we have been sent new details from the add or edit page
+    if (req.body.action === 'Update') {
+      person.set('fullname', req.body.fullname)
+      if (('idontknowbirthday' in req.body && req.body.idontknowbirthday === 'true') || req.body.day === '') {
+        person.set('month', null)
+        person.set('day', null)
+      } else {
+        person.set('month', parseInt(req.body.month, 10))
+        person.set('day', parseInt(req.body.day, 10))
+      }
+      person.set('address', req.body.address)
+      person.set('country', req.body.country)
+
+      const nameSplit = req.body.fullname.split(' ')
+      if (nameSplit.length > 0) person.set('firstname', nameSplit[0])
+      if (nameSplit.length > 1) person.set('lastname', nameSplit[1])
+    }
+
+    //  If we are adding a new contact information thingy
+    if (req.body.action === 'Add contacted' && 'newcontact' in req.body && req.body.newcontact !== '') {
+      person.setContacted(req.body.newcontact)
+    }
+
+    //  If we have been told to save the check values
+    if ('ids' in req.body) {
+      if (!Array.isArray(req.body.ids)) req.body.ids = [req.body.ids]
+      req.body.ids.forEach((id) => {
+        const checkboxes = {
+          checkbox1: false,
+          checkbox2: false
+        }
+        if (`checkbox1_${id}` in req.body) checkboxes.checkbox1 = true
+        if (`checkbox2_${id}` in req.body) checkboxes.checkbox2 = true
+        person.setCheckboxes(checkboxes, id)
+      })
+    }
+    //  If we have been told to delete the contact
+    const actionSplit = req.body.action.split('_')
+    if (actionSplit.length === 2 && actionSplit[0] === 'remove') {
+      person.deleteContacted(actionSplit[1])
+    }
   }
-
   person.set('updated', new Date())
   person.save()
   return res.redirect(`/person/${req.params.id}`)
