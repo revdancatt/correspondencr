c6e3ee6bc72036f9671530cd9ad64b68a26cbb5f
revdancatt <github@revdancatt.com>
2018-08-22 21:04:55 +0100

    Fix discovered people

--- src/routes/discovered/index.js
+++ src/routes/discovered/index.js
@@ -2,6 +2,8 @@ const People = require('../../classes/people')
 
 exports.index = (req, res) => {
   //  Grab all the latest people we have found in the last week
-  req.templateValues.newPeople = new People().getDiscoveredExternally()
+  const days = 30
+  req.templateValues.newPeople = new People().getDiscoveredExternally(days)
+  req.templateValues.days = days
   return res.render('discovered/index', req.templateValues)
 }
