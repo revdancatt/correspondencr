a9eac23d7538c195bfe99c6a21b8e2be71be893e
revdancatt <github@revdancatt.com>
2018-09-02 22:37:24 +0100

    Do handshake

--- src/routes/index.js
+++ src/routes/index.js
@@ -33,12 +33,13 @@ router.use(function (req, res, next) {
 
   //  If there is no username/password in conf then we need to redirect to
   //  the login page
-  if ((configObj.get('username') === null || configObj.get('password') === null || configObj.get('password') === '') && req.url !== '/login') {
+  const urlFragment = req.url.split('?')[0]
+  if ((configObj.get('username') === null || configObj.get('password') === null || configObj.get('password') === '') && urlFragment !== '/login') {
     return res.redirect('/login')
   }
 
   //  If there's no user session then we redirect to the login page
-  if ((!('loggedin' in req.session) || req.session.loggedin === false) && req.url !== '/login') {
+  if ((!('loggedin' in req.session) || req.session.loggedin === false) && urlFragment !== '/login') {
     return res.redirect('/login')
   }
 
